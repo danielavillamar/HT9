e01802c80f4ccf2fc8ea05ffb9852ebb9e10a24c
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-04-22 16:27:02 -0600

    Update BSTFactory.java

--- src/BSTFactory.java
+++ src/BSTFactory.java
@@ -1,3 +1,8 @@
+// DANIELA VILLAMAR 19086
+// MIRKA MONZON 
+// 22/04/2020
+// HT9
+
 public class BSTFactory {
 	//Se crea una instancia o de Red Black Tree o de Splay Tree
 	public BSTInterface<String, String> create(String n){
